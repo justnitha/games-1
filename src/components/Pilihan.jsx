615c635aff87aa3ab17f0772b6582b83aa4125b6
Your Name <you@example.com>
2023-11-20 14:28:45 +0800

    menambahkan responsive

--- src/components/Pilihan.jsx
+++ src/components/Pilihan.jsx
@@ -16,60 +16,60 @@ export default function Pilihan({
     >
       <div className="flex justify-center">
         <div
-          className="atur lg:p-5 iphone6:p-4 iphone6:-mt-8 bg-gradient-to-r cursor-pointer from-orange-11 to-orange-12 rounded-full"
+          className="atur lg:p-5 2xl:p-8 iphone6:p-4 iphone6:-mt-8 bg-gradient-to-r cursor-pointer from-orange-11 to-orange-12 rounded-full"
           onClick={() => handleKlikIcon("scissors")}
         >
-          <div className="bg-white iphone6:p-3 lg:p-2 rounded-full">
+          <div className="bg-white iphone6:p-3 lg:p-2 2xl:p-5 rounded-full">
             <div
-              className=" bg-center bg-no-repeat  lg:h-[100px] lg:w-[100px] iphone6:w-[60px] iphone6:h-[60px]"
+              className=" bg-center bg-no-repeat  lg:h-[100px] lg:w-[100px] iphone6:w-[60px] iphone6:h-[60px] 2xl:w-[120px] 2xl:h-[120px]"
               style={{ backgroundImage: `url(${scissorsIcon})` }}
             ></div>
           </div>
         </div>
       </div>
-      <div className="flex gap-[6rem] lg:gap-[13rem] lg:-mt-6 iphone6:-mt-5 justify-center">
+      <div className="flex gap-[6rem] lg:gap-[13rem] 2xl:-mt-3 lg:-mt-6 iphone6:-mt-5 justify-center">
         <div
-          className="lg:p-5 iphone6:p-4 bg-gradient-to-r cursor-pointer from-cyan-11 to-cyan-12 rounded-full"
+          className="lg:p-5 2xl:p-8 iphone6:p-4 bg-gradient-to-r cursor-pointer from-cyan-11 to-cyan-12 rounded-full"
           onClick={() => handleKlikIcon("spock")}
         >
-          <div className="bg-white iphone6:p-3 lg:p-2 rounded-full">
+          <div className="bg-white iphone6:p-3 lg:p-2 2xl:p-5 rounded-full">
             <div
-              className=" bg-center bg-no-repeat  lg:h-[100px] lg:w-[100px] iphone6:w-[60px] iphone6:h-[60px]"
+              className=" bg-center bg-no-repeat  lg:h-[100px] lg:w-[100px] iphone6:w-[60px] iphone6:h-[60px] 2xl:w-[120px] 2xl:h-[120px]"
               style={{ backgroundImage: `url(${spockIcon})` }}
             ></div>
           </div>
         </div>
         <div
-          className="lg:p-5 iphone6:p-4 bg-gradient-to-r cursor-pointer from-blue-11 to-blue-12 rounded-full"
+          className="lg:p-5 2xl:p-8 iphone6:p-4 bg-gradient-to-r cursor-pointer from-blue-11 to-blue-12 rounded-full"
           onClick={() => handleKlikIcon("paper")}
         >
-          <div className="bg-white iphone6:p-3 lg:p-2 rounded-full">
+          <div className="bg-white iphone6:p-3 lg:p-2 2xl:p-5 rounded-full">
             <div
-              className=" bg-center bg-no-repeat  lg:h-[100px] lg:w-[100px] iphone6:w-[60px] iphone6:h-[60px]"
+              className=" bg-center bg-no-repeat  lg:h-[100px] lg:w-[100px] iphone6:w-[60px] iphone6:h-[60px] 2xl:w-[120px] 2xl:h-[120px]"
               style={{ backgroundImage: `url(${paperIcon})` }}
             ></div>
           </div>
         </div>
       </div>
-      <div className="flex gap-[2rem] lg:gap-[4rem] lg:mt-9 iphone6:mt-5 justify-center">
+      <div className="flex gap-[2rem] lg:gap-[4rem]  lg:mt-9 iphone6:mt-5 justify-center">
         <div
-          className="lg:p-5 iphone6:p-4 bg-gradient-to-t cursor-pointer from-purple-11 to-purple-12 rounded-full shadow-purple-12"
+          className="lg:p-5 2xl:p-8 iphone6:p-4 bg-gradient-to-t cursor-pointer from-purple-11 to-purple-12 rounded-full shadow-purple-12"
           onClick={() => handleKlikIcon("lizard")}
         >
-          <div className="bg-white iphone6:p-3 lg:p-2 rounded-full">
+          <div className="bg-white iphone6:p-3 lg:p-2 2xl:p-5 rounded-full">
             <div
-              className=" bg-center bg-no-repeat  lg:h-[100px] lg:w-[100px] iphone6:w-[60px] iphone6:h-[60px]"
+              className=" bg-center bg-no-repeat  lg:h-[100px] lg:w-[100px] iphone6:w-[60px] iphone6:h-[60px] 2xl:w-[120px] 2xl:h-[120px]"
               style={{ backgroundImage: `url(${LizardIcon})` }}
             ></div>
           </div>
         </div>
         <div
-          className="lg:p-5 iphone6:p-4 bg-gradient-to-t cursor-pointer from-pink-11 to-pink-12 rounded-full shadow-pink-12"
+          className="lg:p-5 2xl:p-8 iphone6:p-4 bg-gradient-to-t cursor-pointer from-pink-11 to-pink-12 rounded-full shadow-pink-12"
           onClick={() => handleKlikIcon("rock")}
         >
-          <div className="bg-white iphone6:p-3 lg:p-2 rounded-full">
+          <div className="bg-white iphone6:p-3 lg:p-2 2xl:p-5 rounded-full">
             <div
-              className=" bg-center bg-no-repeat  lg:h-[100px] lg:w-[100px] iphone6:w-[60px] iphone6:h-[60px]"
+              className=" bg-center bg-no-repeat  lg:h-[100px] lg:w-[100px] iphone6:w-[60px] iphone6:h-[60px] 2xl:w-[120px] 2xl:h-[120px]"
               style={{ backgroundImage: `url(${rockIcon})` }}
             ></div>
           </div>
--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -36,7 +36,7 @@ export default function Games() {
                     {rules ? (
                         <>
                             <div className='bg-black opacity-60 absolute top-0 left-0 w-full h-full z-30'></div>
-                            <div className='w-full lg:w-[30%] lg:h-[29rem] h-full iphone6:h-[120vh] absolute top-0 lg:top-[9.5rem] lg:rounded-md lg:left-[35%] bg-white px-10 pb-16 z-40'>
+                            <div className='w-full lg:w-[30%] lg:h-[29rem] 2xl:h-[45rem] h-full iphone6:h-[120vh] absolute top-0 lg:top-[9.5rem] lg:rounded-md lg:left-[35%] bg-white px-10 pb-16 z-40'>
                                 <div className='lg:flex justify-between items-center py-7'>
                                     <h1 className='text-3xl font-bold text-center iphone6:mt-[5%] mt-[50%] lg:mt-0'>RULES</h1>
                                     <img 
@@ -49,7 +49,7 @@ export default function Games() {
                                 <img 
                                     src={require('../assets/image-rules-bonus.svg').default} 
                                     alt="rules"
-                                    className='w-full mt-10 lg:mt-0' 
+                                    className='w-full mt-10 lg:mt-0 2xl:block' 
                                 />
                                  <img 
                                     src={require('../assets/icon-close.svg').default} 
